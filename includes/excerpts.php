82064ca36a780d3722b9c8a5b83a325d4e851275
Keith Solomon <keith@vengeance.keithsolomon.net>
2014-04-11 09:49:55 -0500

    Upgraded excerpt functions to new WP format.

--- includes/excerpts.php
+++ includes/excerpts.php
@@ -5,15 +5,25 @@
 	This file is a part of the Solo-Frame WordPress theme framework.
 */
 
+function sf_more_link() {
+	return '<br /><a href="'. esc_url(get_permalink()) . '">Read More &raquo;</a>';
+}
+
 function sf_fix_excerpt($more) {
-	return '<br /><a class="more-link" href="'. get_permalink($post->ID) . '">Read More &raquo;</a>';
+	return sf_more_link();
+}
+
+function sf_excerpt_more($output) {
+	$output .= sf_more_link();
 }
 
 function sf_excerpt_length($length) {
 	return 50;
 }
 
-add_filter('excerpt_length', 'sf_excerpt_length');
+add_filter('get_the_excerpt', 'sf_excerpt_more',1);
 
-add_filter('excerpt_more', 'sf_fix_excerpt');
+add_filter('excerpt_length', 'sf_excerpt_length',1);
+
+add_filter('excerpt_more', 'sf_fix_excerpt',1);
 ?>
