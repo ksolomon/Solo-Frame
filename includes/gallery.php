de950774c0f464fc17de6a6ae64063cf67273e0a
Keith Solomon <keith@solowebdeisgns.net>
2014-09-02 10:19:38 -0500

    Updated custom gallery function to work with new version of WP

--- includes/gallery.php
+++ includes/gallery.php
@@ -1,70 +1,53 @@
 <?php
-//deactivate WordPress function
-remove_shortcode('gallery', 'gallery_shortcode');
+add_filter('post_gallery', 'sf_gallery', 10, 2);
 
-//activate own function
-add_shortcode('gallery', 'sf_gallery_shortcode');
+function sf_gallery($output, $attr) {
+    global $post, $wp_locale;
 
-//the own renamed function
-function sf_gallery_shortcode($attr) {
-	global $post;
+    static $instance = 0;
+    $instance++;
 
-	static $instance = 0;
-	$instance++;
+    extract(shortcode_atts(array(
+        'order'      => 'ASC',
+        'orderby'    => 'menu_order ID',
+        'id'         => $post->ID,
+        'itemtag'    => 'div',
+        'icontag'    => 'div',
+        'captiontag' => 'div',
+        'columns'    => 3,
+        'size'       => 'thumbnail',
+        'include'    => '',
+        'exclude'    => ''
+   ), $attr));
 
-	// Allow plugins/themes to override the default gallery template.
-	$output = apply_filters('post_gallery', '', $attr);
-	if ($output != '')
-		return $output;
+    $id = intval($id);
+    if ('RAND' == $order)
+        $orderby = 'none';
 
-	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
-	if (isset($attr['orderby'])) {
-		$attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
-		if (!$attr['orderby'])
-			unset($attr['orderby']);
-	}
+    if (!empty($include)) {
+        $include = preg_replace('/[^0-9,]+/', '', $include);
+        $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
 
-	extract(shortcode_atts(array(
-		'order'      => 'ASC',
-		'orderby'    => 'menu_order ID',
-		'id'         => $post->ID,
-		'itemtag'    => 'dl',
-		'icontag'    => 'dt',
-		'captiontag' => 'dd',
-		'columns'    => 3,
-		'size'       => 'thumbnail',
-		'ids'    => '',
-		'exclude'    => ''
-	), $attr));
+        $attachments = array();
+        foreach ($_attachments as $key => $val) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    } elseif (!empty($exclude)) {
+        $exclude = preg_replace('/[^0-9,]+/', '', $exclude);
+        $attachments = get_children(array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+    } else {
+        $attachments = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+    }
 
-	$id = intval($id);
-	if ('RAND' == $order)
-		$orderby = 'none';
+    if (empty($attachments))
+        return '';
 
-	if (!empty($ids)) {
-		$ids = preg_replace('/[^0-9,]+/', '', $ids);
-		$_attachments = get_posts(array('include' => $ids, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
-
-		$attachments = array();
-		foreach ($_attachments as $key => $val) {
-			$attachments[$val->ID] = $_attachments[$key];
-		}
-	} elseif (!empty($exclude)) {
-		$exclude = preg_replace('/[^0-9,]+/', '', $exclude);
-		$attachments = get_children(array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
-	} else {
-		$attachments = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
-	}
-
-	if (empty($attachments))
-		return '';
-
-	if (is_feed()) {
-		$output = "\n";
-		foreach ($attachments as $att_id => $attachment)
-			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
-		return $output;
-	}
+    if (is_feed()) {
+        $output = "\n";
+        foreach ($attachments as $att_id => $attachment)
+            $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+        return $output;
+    }
 
 	$itemtag = tag_escape($itemtag);
 	$captiontag = tag_escape($captiontag);
@@ -87,7 +70,7 @@ function sf_gallery_shortcode($attr) {
     foreach ($attachments as $id => $attachment) {
 		$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
 
-		$output .= "<{$itemtag} class='gallery-item col-{$columns}'>";
+		$output .= "<{$itemtag} id='item-{$id}' class='gallery-item col-{$columns}'>";
 		$output .= "<{$icontag} class='gallery-icon'>$link</{$icontag}>";
 
 		if ($captiontag && trim($attachment->post_excerpt)) {
@@ -102,6 +85,6 @@ function sf_gallery_shortcode($attr) {
 
 	$output .= "</div>\n";
 
-   	return $output;
+    return $output;
 }
 ?>
