ae286a966a49d676ba05f3403dd5dd42cebefff0
Keith Solomon <keith@solowebdeisgns.net>
2015-02-02 10:22:43 -0600

    Removed extraneous spaces before and after parentheses

--- comments.php
+++ comments.php
@@ -3,7 +3,7 @@
 	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
 		die ('Please do not load this page directly. Thanks!');
 
-	if ( post_password_required() ) { ?>
+	if (post_password_required()) { ?>
 		<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
 	<?php
 		return;
@@ -11,16 +11,16 @@
 ?>
 
 <?php //You can start editing here. ?>
-<?php if ( have_comments() ) : ?>
-	<?php if ( ! empty($comments_by_type['comment']) ) : ?>
-		<h3><?php comments_number('No Comments', '1 Comment', '% Comments' );?> on &#8220;<?php the_title(); ?>&#8221;</h3>
+<?php if (have_comments()) : ?>
+	<?php if (! empty($comments_by_type['comment'])) : ?>
+		<h3><?php comments_number('No Comments', '1 Comment', '% Comments');?> on &#8220;<?php the_title(); ?>&#8221;</h3>
 
 		<ul class="commentlist">
 			<?php wp_list_comments('type=comment'); ?>
 		</ul>
 	<?php endif; ?>
 
-	<?php if ( ! empty($comments_by_type['pings']) ) : ?>
+	<?php if (! empty($comments_by_type['pings'])) : ?>
 		<h3 id="pings">Trackbacks/Pingbacks</h3>
 
 		<ul class="pinglist">
@@ -54,17 +54,17 @@
 
 <?php if ('open' == $post->comment_status) : ?>
 	<div id="respond">
-		<h3><?php comment_form_title( 'Post a Comment', 'Post a Comment on %s' ); ?></h3>
+		<h3><?php comment_form_title('Post a Comment', 'Post a Comment on %s'); ?></h3>
 
 		<div class="cancel-comment-reply">
 			<small><?php cancel_comment_reply_link(); ?></small>
 		</div>
 
-		<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
+		<?php if (get_option('comment_registration') && !$user_ID) : ?>
 			<p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p>
 		<?php else : ?>
 			<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-				<?php if ( $user_ID ) : ?>
+				<?php if ($user_ID) : ?>
 					<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out &raquo;</a></p>
 				<?php else : ?>
 					<p>
