acab95e5392d5ad06f459de54497cb6820a32bfa
Keith Solomon <ksolomon@gmail.com>
2022-09-22 12:41:12 -0500

    Chore(*): Legacy code cleanup
    Removed:
     flexibility.js
     classie.js
     css_brwoser_selector.js
     modernizr.js
     sidebarEffects.js
     nav_walker.php
    Removed legacy scripts from scripts.js
    Removed wrapper code for legacy hamburger menu

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
 
 	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
 
-
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
@@ -17,8 +16,6 @@
 	<?php if (is_single() || is_page()) { ?> <link rel="canonical" href="<?php the_permalink(); ?>" /> <?php } ?>
 
 	<?php wp_head(); ?>
-
-	<script src="<?php bloginfo('template_url'); ?>/includes/flexibility.js"></script>
 
 	<!-- Global style reset and WP basic styles -->
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/reset.css?v=<?php echo filemtime(TEMPLATEPATH . '/css/reset.css'); ?>" media="screen, projection" />
@@ -28,44 +25,19 @@
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css?v=<?php echo filemtime(TEMPLATEPATH . '/style.css'); ?>" media="screen, projection" />
 
 	<!-- Mobile styles -->
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/mobile-nav.css?v=<?php echo filemtime(TEMPLATEPATH . '/css/mobile-nav.css'); ?>" media="screen, projection" />
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/responsive.css?v=<?php echo filemtime(TEMPLATEPATH . '/css/responsive.css'); ?>" media="screen, projection" />
 </head>
 
 <body <?php body_class(); ?>>
-	<div id="st-container" class="st-container">
-		<!-- content push wrapper -->
-		<div class="st-pusher">
-			<nav class="st-menu st-effect-3" id="menu-3">
-				<!-- menu content -->
-				<h2 class="icon icon-lab">Menu</h2>
+	<!-- wrapper -->
+	<div id="wrapper" class="frame">
+		<header id="header" role="banner">
+			<hgroup id="logo">
+				<h1 id="site-name"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</hgroup>
 
-				<?php
-				wp_nav_menu( array(
-					'theme_location'    => 'mobile',
-					'depth'             => 2,
-					'container'         => 'div',
-					'container_class'   => 'collapse navbar-collapse navbar-1-collapse',
-					'menu_class'        => 'nav navbar-nav',
-					'fallback_cb'       => 'sf_navwalker::fallback',
-					'walker'            => new sf_navwalker())
-				);
-				?>
+			<nav id="nav" role="navigation">
+				<?php wp_nav_menu(array('theme_location' => 'primary')); ?>
 			</nav>
-
-			<div id="st-trigger-effects"><button data-effect="st-effect-3">Menu</button></div>
-
-			<div class="st-content"><!-- this is the wrapper for the content -->
-				<div class="st-content-inner"><!-- extra div for emulating position:fixed of the menu -->
-					<!-- wrapper -->
-					<div id="wrapper" class="frame">
-						<header id="header" role="banner">
-							<hgroup id="logo">
-								<h1 id="site-name"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-								<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-							</hgroup>
-
-							<nav id="nav" role="navigation">
-								<?php wp_nav_menu(array('theme_location' => 'primary')); ?>
-							</nav>
-						</header>
+		</header>
